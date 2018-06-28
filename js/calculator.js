102bfdf5ccb61cb85544caa5b470448bda896053
Jason <Jason@Colins-MacBook-Pro.local>
2018-06-27 15:34:42 -1000

    completed equalsFunction operators and double digits functionality

---
+++ js/calculator.js
@@ -0,0 +1,205 @@
+(function() {
+    // var numbersButtonClass = document.getElementsByClassName('number-button');
+    // for(var i = 0; i < numbersButtonClass.length; i++) {
+    //     numbersButtonClass[i].addEventListener('click', function(){numbersOnScreen(i)});
+    // }  
+    // var numbersOnScreen = function(num) {
+    //     var value = num;
+    //     console.log(value);
+    // }
+    var addScreenStore;
+    var subScreenStore;
+    var equalsScreenStore;
+    var divScreenStore;
+    var multiplyScreenStore;
+
+    calScreen.innerHTML = null;
+
+    var operations = document.getElementsByClassName('operator')
+    // console.log(operations)
+    var systemsButtons = document.getElementsByClassName('systems-buttons');
+    console.log(systemsButtons)
+
+
+    //adding number id's and their event listeners
+    var sevenId = document.getElementById('seven');
+    sevenId.addEventListener('click', function() {
+        if(calScreen.innerHTML === '-' || calScreen.innerHTML === '+' || calScreen.innerHTML === 'x' || calScreen.innerHTML === '/') {
+            calScreen.innerHTML = null;
+        }
+            calScreen.innerHTML = calScreen.innerHTML + 7;
+    })
+
+    var fourId = document.getElementById('four');
+    fourId.addEventListener('click', function() {
+        if(calScreen.innerHTML === '-' || calScreen.innerHTML === '+' || calScreen.innerHTML === 'x' || calScreen.innerHTML === '/') {
+            calScreen.innerHTML = null;
+        }
+        calScreen.innerHTML = calScreen.innerHTML + 4;
+    })
+
+    var oneId = document.getElementById('one');
+    oneId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 1;
+    })
+
+    var zeroId = document.getElementById('zero');
+    zeroId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 0;
+    })
+
+    var eightId = document.getElementById('eight');
+    eightId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 8;
+    })
+
+    var fiveId = document.getElementById('five');
+    fiveId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 5;
+    })
+
+    var twoId = document.getElementById('two');
+    twoId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 2;
+    })
+
+    var decimalId = document.getElementById('decimal');
+    decimalId.addEventListener('click', function() {
+        calScreen.innerHTML = '.';
+    })
+    var nineId = document.getElementById('nine');
+    nineId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 9;
+    })
+
+    var sixId = document.getElementById('six');
+    sixId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 6;
+    })
+
+    var threeId = document.getElementById('three');
+    threeId.addEventListener('click', function() {
+        calScreen.innerHTML = calScreen.innerHTML + 3;
+    })
+    
+    //adding systems buttons and their id's
+    var clearButton = systemsButtons[2];
+    clearButton.addEventListener('click', function() {
+        calScreen.innerHTML = null;
+    })
+
+
+    operations[0].addEventListener('click', equalsFunction); //add eventListener
+    operations[4].addEventListener('click', addFunction); //add button
+    operations[3].addEventListener('click', subtractFunction); //subtract eventListener
+    operations[1].addEventListener('click', divideFunction); //divide eventListener
+    operations[2].addEventListener('click', multiplyFunction); //multiply eventListener
+    
+    //operator functions:
+    function addFunction() { //addition function
+        addScreenStore = parseFloat(calScreen.innerHTML);
+        calScreen.innerHTML = '+';
+    }
+    
+    function subtractFunction() { //subtract function
+        subScreenStore = parseFloat(calScreen.innerHTML);
+        calScreen.innerHTML = '-';
+    }
+
+    function divideFunction() { //divide function
+        divScreenStore = parseFloat(calScreen.innerHTML);
+        calScreen.innerHTML = '/';
+    }
+
+    function multiplyFunction() { //multiply function
+        multiplyScreenStore = parseFloat(calScreen.innerHTML);
+        calScreen.innerHTML = 'x';
+    }
+
+
+    function equalsFunction() {
+        if(addScreenStore) {
+            equalsScreenStore = parseFloat(calScreen.innerHTML);
+            var addResult = addScreenStore + equalsScreenStore;
+            calScreen.innerHTML = addResult;
+            addScreenStore = null;
+        }
+        else if(subScreenStore) {
+            equalsScreenStore = parseFloat(calScreen.innerHTML);
+            var subResult = subScreenStore - equalsScreenStore;
+            calScreen.innerHTML = subResult;
+            subScreenStore = null;
+        }
+        else if(divScreenStore) {
+            equalsScreenStore = parseFloat(calScreen.innerHTML);
+            var divideResult = divScreenStore / equalsScreenStore;
+            calScreen.innerHTML = divideResult;
+            divScreenStore = null;
+        }
+        else if(multiplyScreenStore) {
+            equalsScreenStore = parseFloat(calScreen.innerHTML);
+            var multiplyResult = multiplyScreenStore * equalsScreenStore;
+            calScreen.innerHTML = multiplyResult;
+            multiplyScreenStore = null;
+        }
+        
+    }
+
+  
+
+
+// var calculatorModule = (function() {
+//     var memory = 0;
+//     var total = 0;
+
+//     var load = function(num) {
+//         if(typeof num === 'number') {
+//             total = num;
+//             return total;
+//         } else {
+//             throw err;
+//         }
+//     }
+//     var getTotal = function() {
+//         return total;
+//     }
+//     var add = function(num) {
+//         if(typeof num === 'number') {
+//             total+= num;
+//         } else {
+//             throw err;
+//         }
+//     var subtract = function(num) {
+//         if(typeof num === 'number') {
+//         total -= num;
+//         } else {
+//             throw err;
+//         }
+//     }
+//     var multiply = function(num) {
+//         if(typeof num === 'number') {
+//         total *= num;
+//         } else {
+//             throw err;
+//         }
+//     }
+//     var divide = function(num) {
+//         if(typeof num === 'number') {
+//         total /= num;
+//         } else {
+//             throw err;
+//         }
+//     }
+//     var recallMemory = function() {
+//         return memory;
+//     }
+//     var saveMemory = function() {
+//         memory = total;
+//     }
+//     var clearMemory = function() {
+//         memory = 0;
+//     }
+    // var validate = function(operation) {
+    //     typeof operation 
+    // }
+}());
