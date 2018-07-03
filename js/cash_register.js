7056ee7fb79441a6bf30ec625612b3b310a79562
Jason Hoe <jasonhoe928@gmail.com>
2018-07-02 15:09:02 -1000

    fixed withdrawal when negative balance

--- js/cash_register.js
+++ js/cash_register.js
@@ -11,12 +11,12 @@ cashButtonsClass[0].addEventListener('click', function() { //deposit addEventLis
     else if(depositStorage && balanceStorage) {
         depositStorage = parseFloat(calScreen.innerHTML);
         balanceStorage += depositStorage;
-        calScreen.innerHTML = '$' + depositStorage + ' deposited';  
+        calScreen.innerHTML = '$' + depositStorage + ' deposited';
      }
      else if(depositStorage && !balanceStorage) {
         depositStorage = parseFloat(calScreen.innerHTML);
         balanceStorage += depositStorage;
-        calScreen.innerHTML = '$' + depositStorage + ' deposited'; 
+        calScreen.innerHTML = '$' + depositStorage + ' deposited';
      }
     setTimeout(function() {
         calScreen.innerHTML = null;
@@ -25,11 +25,16 @@ cashButtonsClass[0].addEventListener('click', function() { //deposit addEventLis
 
 cashButtonsClass[1].addEventListener('click', function() { //withdrawal addEventListener
     var withdrawalStorage = parseFloat(calScreen.innerHTML);
-    if(!balanceStorage) {
-       calScreen.innerHTML = 'Insufficient funds';
+    if(balanceStorage > withdrawalStorage) {
+        if (!balanceStorage) {
+            calScreen.innerHTML = 'Insufficient funds';
+        }
+        else {
+            balanceStorage = balanceStorage - withdrawalStorage;
+            calScreen.innerHTML = '$' + withdrawalStorage + ' withdrawn';
+        }
     } else {
-        balanceStorage = balanceStorage - withdrawalStorage;
-        calScreen.innerHTML = '$' + withdrawalStorage + ' withdrawn';
+        calScreen.innerHTML = 'Insufficient funding';
     }
     setTimeout(function() {
         calScreen.innerHTML = null;
